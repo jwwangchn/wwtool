dec4f656aff33de6e54249636ceee8bfa40fdf96
jwwangchn <jwwangchn@outlook.com>
2019-07-26 23:32:58 +0800

    add type convert

--- wwtool/ops/bbox/bbox_wrapper.py
+++ wwtool/ops/bbox/bbox_wrapper.py
@@ -5,6 +5,14 @@ from .bbox_overlaps_cpu import bbox_overlaps_cpu
 
 
 def iou(bboxes1, bboxes2, mode='iou', is_aligned=False):
+    if isinstance(bboxes1, torch.Tensor):
+        bboxes1 = bboxes1.numpy()
+        bboxes2 = bboxes2.numpy()
+    elif isinstance(bboxes1, np.ndarray):
+        pass
+    else:
+        raise TypeError('bboxes1, bboxes2 must be either a Tensor or numpy array, but got {}'.format(type(bboxes1)))
+
     ious = bbox_overlaps_cpu(bboxes1, bboxes2, mode, is_aligned)
 
     return ious

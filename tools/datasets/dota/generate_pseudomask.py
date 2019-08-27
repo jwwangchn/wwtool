87d7b26395bb224dc7e2a502bfa3540f1a1a9e19
jwwangchn <jwwangchn@outlook.com>
2019-08-27 09:41:46 +0800

    finish merge pseudomask and seg

--- tools/datasets/dota/generate_pseudomask.py
+++ tools/datasets/dota/generate_pseudomask.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
     pointobb_sort_method = 'best'
     extra_info = 'keypoint'
 
-    encode = 'gaussmask'   # centernessmask, gaussmask, ellipsemask
+    encode = 'ellipsemask'   # centernessmask, gaussmask, ellipsemask
 
     save_vis = True
     show_pseudomask = False

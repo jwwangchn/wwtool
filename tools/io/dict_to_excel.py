a672de153c8ea0e6e31e0bd1daeac3202cc40808
jwwangchn <jwwangchn@outlook.com>
2019-09-17 11:16:00 +0800

    sync

--- tools/io/dict_to_excel.py
+++ tools/io/dict_to_excel.py
@@ -1,4 +1,5 @@
 import wwtool
 
-data = {'plane': 0.883057839736689, 'baseball-diamond': 0.84474761747509, 'bridge': 0.57719902396708, 'ground-track-field': 0.6037899238470824, 'small-vehicle': 0.7818678604144524, 'large-vehicle': 0.6645229286572404, 'ship': 0.7896737475632843, 'tennis-court': 0.8923527250660408, 'basketball-court': 0.8237293942903094, 'storage-tank': 0.83750244268759, 'soccer-ball-field': 0.47451016995183687, 'roundabout': 0.6627236251577227, 'harbor': 0.7168307302432708, 'swimming-pool': 0.7915134424481486, 'helicopter': 0.6397739828140536}
-wwtool.dict2excel(data) 
+data = {'plane': 0.8845910352875399, 'baseball-diamond': 0.8203372112087907, 'bridge': 0.5603849116292413, 'ground-track-field': 0.6158651080115715, 'small-vehicle': 0.787096759243718, 'large-vehicle': 0.6635556247012467, 'ship': 0.790617895644017, 'tennis-court': 0.895281746334651, 'basketball-court': 0.7867441947364027, 'storage-tank': 0.8285766600962888, 'soccer-ball-field': 0.41862243960797096, 'roundabout': 0.6577382487596026, 'harbor': 0.7267029354718022, 'swimming-pool': 0.7351054412392668, 'helicopter': 0.5999020350746267}
+
+wwtool.dict2excel(data)

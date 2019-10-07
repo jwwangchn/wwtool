63999cac2a2b1a7b5f27431cdb6be70c4c7aef18
jwwangchn <jwwangchn@outlook.com>
2019-10-08 00:03:49 +0800

    sync

--- tools/datasets/dota/generate_obb_seg.py
+++ tools/datasets/dota/generate_obb_seg.py
@@ -70,7 +70,7 @@ class Core():
                 self.progress_bar.update()
 
 if __name__ == '__main__':
-    core_dataset = 'hrsc'
+    core_dataset = 'dota'
     release_version = 'v1'
     imageset = 'trainval'
     rate = '1.0'

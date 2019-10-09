62a3530827d363fc2c93f34cb04338eb99310011
jwwangchn <jwwangchn@outlook.com>
2019-10-09 11:58:32 +0800

    fix the sorted bug

--- tools/datasets/gaofen/generate_dataset.py
+++ tools/datasets/gaofen/generate_dataset.py
@@ -3,4 +3,4 @@ import wwtool
 origin_dataset_dir = '/data/gaofen/v1/origin'
 trainval_dir = '/data/gaofen/v1/trainval'
 test_dir = '/data/gaofen/v1/test'
-wwtool.shuffle_dataset(origin_dataset_dir, trainval_dir, test_dir)
+wwtool.shuffle_dataset(origin_dataset_dir, trainval_dir, test_dir) 

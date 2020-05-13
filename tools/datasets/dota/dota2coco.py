a6b9b16630a402df7eddaf526d6db55a2e287e08
jwwangchn <jwwangchn@outlook.com>
2020-05-13 09:28:33 +0800

    sync deepmind

--- tools/datasets/dota/dota2coco.py
+++ tools/datasets/dota/dota2coco.py
@@ -180,11 +180,11 @@ if __name__ == "__main__":
                             {'supercategory': 'none', 'id': 14, 'name': 'ground-track-field',    },
                             {'supercategory': 'none', 'id': 15, 'name': 'basketball-court',      },]
 
-    core_dataset_name = 'dota-v1.0'
-    imagesets = ['test']
+    core_dataset_name = 'dota'
+    imagesets = ['trainval']
     dota_version = 'v1.0'
     release_version = 'DJ'
-    groundtruth = False
+    groundtruth = True
     single_category = None
     keypoint = False
 

080b9865b4febddf853fc383f2604a10ebeb405f
jwwangchn <jwwangchn@outlook.com>
2020-05-25 10:28:49 +0800

    sync jwwangchn codes on

--- wwtool/datasets/class_label.py
+++ wwtool/datasets/class_label.py
@@ -11,3 +11,10 @@ class Small():
 
     def full2abbr(self, names):
         return [self.abbr[name] for name in names]
+
+class DOTA():
+    def __init__(self):
+        self.abbr = {'harbor': 'HA', 'ship': 'SH', 'small-vehicle': 'SV', 'large-vehicle': 'LV', 'storage-tank': 'ST', 'plane': 'PL', 'soccer-ball-field': 'SBF', 'bridge': 'BR', 'baseball-diamond': 'BD', 'tennis-court': 'TC', 'helicopter': 'HC', 'roundabout': 'RA', 'swimming-pool': 'SP', 'ground-track-field': 'GTF', 'basketball-court': 'BC'}
+
+    def full2abbr(self, names):
+        return [self.abbr[name] for name in names]

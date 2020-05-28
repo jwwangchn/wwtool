11c9f20e24a8b266cc766cae4f09a39aefe0f3c9
jwwangchn <jwwangchn@outlook.com>
2020-05-28 14:37:38 +0800

    sync jwwangchn codes on

--- wwtool/utils/__init__.py
+++ wwtool/utils/__init__.py
@@ -2,5 +2,6 @@ from .pid import PID
 from .uart import UART
 from .email import Email
 from .path import mkdir_or_exist, get_basename
+from .metrics import SegMetric
 
-__all__ = ['PID', 'UART', 'Email', 'mkdir_or_exist', 'get_basename']
+__all__ = ['PID', 'UART', 'Email', 'mkdir_or_exist', 'get_basename', 'SegMetric']

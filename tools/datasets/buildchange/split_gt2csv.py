cfe516cbcd3779ad9e219101633fea37f123768f
jwwangchn <jwwangchn@outlook.com>
2020-06-11 14:51:46 +0800

    sync jwwangchn codes on

--- tools/datasets/buildchange/split_gt2csv.py
+++ tools/datasets/buildchange/split_gt2csv.py
@@ -4,47 +4,38 @@ import numpy as np
 import rasterio as rio
 import cv2
 import pandas
+import mmcv
 
 
 if __name__ == '__main__':
-    csv_file = './data/buildchange/v2/xian_fine/xian_fine_gt.csv'
-    first_in = True
 
+    keywords = ['roof', 'footprint']
 
-    json_dir = f'./data/buildchange/v2/xian_fine/labels'
-    rgb_img_dir = f'./data/buildchange/v2/xian_fine/images'
+    for key in keywords:
+        csv_file = './data/buildchange/v2/xian_fine/xian_fine_{}_gt.csv'.format(key)
+        first_in = True
 
-    for shp_fn in os.listdir(json_dir):
-        if not shp_fn.endswith('shp'):
-            continue
-        print("Processing: ", shp_fn)
-        base_name = wwtool.get_basename(shp_fn)
+        json_dir = './data/buildchange/v2/xian_fine/labels_json'
+        rgb_img_dir = './data/buildchange/v2/xian_fine/images'
 
-        rgb_img_file = os.path.join(rgb_img_dir, base_name + '.jpg')
-        shp_file = os.path.join(shp_dir, shp_fn)
+        for json_fn in os.listdir(json_dir):
+            base_name = wwtool.get_basename(json_fn)
 
-        rgb_img = cv2.imread(rgb_img_file)
-        geo_info = rio.open(rgb_img_file)
+            rgb_img_file = os.path.join(rgb_img_dir, base_name + '.png')
+            json_file = os.path.join(json_dir, json_fn)
 
-        shp_parser = wwtool.ShpParse()
-        objects = shp_parser(shp_file, 
-                            geo_info,
-                            coord='pixel',
-                            merge_flag=True, 
-                            connection_mode='floor')
+            annotations = mmcv.load(json_file)['annotations']
 
-        gt_polygons = [wwtool.mask2polygon(obj['segmentation']) for obj in objects]
+            masks = [wwtool.mask2polygon(anno[key]) for anno in annotations]
 
-        # wwtool.show_polygons_on_image(gt_masks, rgb_img, output_file=None)
+            csv_image = pandas.DataFrame({'ImageId': base_name,
+                                            'BuildingId': range(len(masks)),
+                                            'PolygonWKT_Pix': masks,
+                                            'Confidence': 1})
+            if first_in:
+                csv_dataset = csv_image
+                first_in = False
+            else:
+                csv_dataset = csv_dataset.append(csv_image)
 
-        csv_image = pandas.DataFrame({'ImageId': sub_fold + '_' + base_name,
-                                        'BuildingId': range(len(gt_polygons)),
-                                        'PolygonWKT_Pix': gt_polygons,
-                                        'Confidence': 1})
-        if first_in:
-            csv_dataset = csv_image
-            first_in = False
-        else:
-            csv_dataset = csv_dataset.append(csv_image)
-
-    csv_dataset.to_csv(csv_file, index=False)
+        csv_dataset.to_csv(csv_file, index=False)

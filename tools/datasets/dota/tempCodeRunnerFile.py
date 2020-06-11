e010adcc3410779ec2de8d97f55b3aac18c3ada7
jwwangchn <jwwangchn@outlook.com>
2020-06-11 14:39:59 +0800

    sync jwwangchn codes on

--- tools/datasets/dota/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-nge, y_zeros, y)
-    # cset = ax.contour(index_x, index_y, centerness_prob, zdir='x', offset=0, cmap=cm.coolwarm)
-    # cset = ax.contour(index_x, index_y, centerness_prob, zdir='y', offset=64, cmap=cm.coolwarm)
- 
